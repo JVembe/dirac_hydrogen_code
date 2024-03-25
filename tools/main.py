98deb490f39278a1f2b1f30d7653804f150a4154
Marcin <marcin.krotkiewski@gmail.com>
2024-03-25 10:42:12 +0100

    save full vector permutation in main.py

--- tools/main.py
+++ tools/main.py
@@ -85,6 +85,10 @@ if nparts > 1:
 
     pyplot.spy(res, marker='.', markersize=1)
     pyplot.show()
+else:
+
+    # block permutation - needed for saving the result vector
+    perm = npy.arange(0, res.shape[0])
 
 res = res_orig[nzc[:, None], nzc]
 
@@ -96,19 +100,20 @@ psi0 = dense_read('psi0')
 print(psi0.dtype)
 
 # make a column permutation vector
-nzc_blk = nzc.reshape((-1,1))
-nzc_blk = npy.tile(nzc_blk, (1, blkdim))
+perm_full = perm.reshape((-1,1))
+perm_full = npy.tile(perm_full, (1, blkdim))
 dof_blk = npy.indices((1,blkdim))[1]
-psi0_perm = nzc_blk*blkdim + dof_blk
-psi0_perm = psi0_perm.reshape((1,-1))
+psi0_perm = perm_full*blkdim + dof_blk
+psi0_perm = psi0_perm.reshape((1,-1)).astype(idxtype)
 psi0 = psi0[psi0_perm]
-print('full dim without empty: ', psi0_perm.shape)
+print('full dim without empty: ', psi0_perm.shape, ' type ', psi0_perm.dtype)
 
 # save permuted psi0
 with open('psi0.vec', 'w+') as f:
     dim = npy.array(psi0_perm.shape[1], idxtype)
     dim.tofile(f)
     psi0.tofile(f)
+    psi0_perm.tofile(f)
 
 # save permuted H structure
 with open('H.csr', 'w+') as f:
