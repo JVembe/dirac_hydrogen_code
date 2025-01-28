3ce0bc622dd673f2e4259e5f6958e84927a0c8c6
Johanne Vembe <johanne.vembe@uib.no>
2025-01-28 11:29:51 +0100

    Added license notices to all source files

--- src/splinehandler.h
+++ src/splinehandler.h
@@ -1,5 +1,27 @@
 /*
-The splineHandler class makes more sense when the Schrödinger code is also included, as it is a parent class of both the pure b-spline basis used in the Schrödinger picture and the dual kinetic balance basis used in the Dirac picture
+	GaDE - Gpu-accelerated solver for the time dependent Dirac Equation
+	
+    Copyright (C) 2025  Johanne Elise Vembe <johannevembe@gmail.com>
+    Copyright (C) 2025  Marcin Krotkiewski <marcink@uio.no>
+	Copyright (C) 2025  Hicham Agueny <hicham.agueny@uib.no>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+The splineHandler class makes more sense when the Schrödinger code is also included, as it is a parent class of both the pure 
+b-spline basis used in the Schrödinger picture and the dual kinetic balance basis used in the Dirac picture
 
 The class contains functionality directly dealing with generating B-splines for use by other bases.
 */
